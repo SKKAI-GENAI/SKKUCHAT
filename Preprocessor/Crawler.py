c6abc3b752f3573d14c5b32911d42438d4aa4e61
Jung Moongyu <113420520+jmoongyu24@users.noreply.github.com>
2025-05-07 18:24:18 +0900

    Mecab text preprocessor files added

---
+++ Preprocessor/Crawler.py
@@ -0,0 +1,77 @@
+import re
+import json
+import requests
+import csv
+import pandas as pd
+import numpy as np
+from bs4 import BeautifulSoup as bs
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+def crawl_skku_notices():
+    NOW_KST = datetime.now(ZoneInfo("Asia/Seoul")).strftime("%Y-%m-%d %H:%M:%S")
+    URL = "https://www.skku.edu/skku/campus/skk_comm/notice01.do"
+    query_list = lambda pg: f"?mode=list&&articleLimit=10&article.offset={pg*10}"
+    query_view = lambda id: f"?mode=view&articleNo={id}"
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+        "Accept-Language": "ko-KR,ko;q=0.9",
+        "Referer": "https://www.skku.edu/",
+    }
+
+    data = []
+
+    for pg in range(33):
+        # 공지사항 리스트 가져오기
+        response = requests.get(URL + query_list(pg), headers=headers)
+        html = bs(response.text, "html.parser")
+
+        headers["Referer"] = URL + query_list(pg)
+
+        notice_list = html.select("dl.board-list-content-wrap ")
+        for notice in notice_list:
+            category = notice.select_one("span.c-board-list-category").get_text(strip=True)
+
+            url = notice.select_one("a")["href"]
+            id = re.search(r"articleNo=(\d+)", url).group(1)
+            title = notice.select_one("a").get_text(strip=True)
+
+            info = notice.select_one("dd.board-list-content-info")
+            created_date = info.select("ul li")[2].get_text(strip=True)
+
+            # 공지사항 게시물 가져오기
+            subresponse = requests.get(URL + query_view(id), headers=headers)
+            subhtml = bs(subresponse.text, "html.parser")
+
+            pre = subhtml.select_one("pre.pre")
+            content = pre.get_text(strip=True) if pre is not None else ""
+
+            data.append(
+                {
+                    "id": id,
+                    "title": title,
+                    "content": content,
+                    "category": category,  # [채용/모집], [행사/세미나] 등
+                    "created_date": created_date,  # 공지 작성일
+                    "crawled_date": NOW_KST,  # 크롤링된 시간
+                    "department": "홈페이지 공지사항",
+                }
+            )
+
+
+    json_data = json.dumps(data, indent=4, ensure_ascii=False)
+
+    # 크롤링 한 내용을 JSON으로 저장
+    with open("skku_notices.json", "w", encoding="utf-8") as f:
+        f.write(json_data)
+
+    # JSON을 CSV로 변환
+    with open("skku_notices.json",'r',encoding='utf-8') as input_file, open("skku_notices.csv", 'w', newline='', encoding='utf-8') as output_file:
+        data=json.load(input_file)
+        f=csv.writer(output_file)
+
+        f.writerow(["id", "title", "content", "category", "created_date", "crawled_date", "department"])  # Write the header row
+        
+        for row in data:
+            f.writerow([row["id"], row["title"], row["content"], row["category"], row["created_date"], row["crawled_date"], row["department"]])
